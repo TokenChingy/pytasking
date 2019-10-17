b81e0232d9cc9f10f12c8ad3a9c5fdd338283dca
Jason Lei <tokenchingy@gmail.com>
2019-10-17 23:46:10 +0800

    Fixed a bug where processors were being started before the manager was started.

--- multitasking/multitasking.py
+++ multitasking/multitasking.py
@@ -12,8 +12,11 @@ class Manager:
 
             asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
         except:
-            logging.info("Python3 module `uvloop` not found.")
+            logging.info(
+                "Python3 module `uvloop` not found, defaulting to standard asyncio event loop policy."
+            )
 
+        self.__started = False
         self.__loop = asyncio.get_event_loop()
         self.__tasks = {}
         self.__processes = {}
@@ -38,9 +41,12 @@ class Manager:
             kwargs=kwargs
         )
         p.daemon = True
-        p.start()
         p_id = hash(p)
         self.__processes[p_id] = p
+
+        if self.__started:
+            p.start()
+
         return p_id
 
     def delete_process(self, p_id):
@@ -48,5 +54,11 @@ class Manager:
         self.__processes[p_id].join()
 
     def start(self):
+        logging.info("Multitasking manager started...")
+
+        for p in self.__processes.values():
+            p.start()
+
+        self.__started = True
         self.__loop.run_until_complete(asyncio.ensure_future(self.__tasks__()))
         self.__loop.close()
